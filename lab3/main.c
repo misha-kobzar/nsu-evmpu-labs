fa7756561b7a8c1845d1987bdb49fdd8859707c3
misha-kobzar <118627088+misha-kobzar@users.noreply.github.com>
2023-05-21 22:33:22 +0700

    upd

--- lab3/main.c
+++ lab3/main.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-	double pi = 0.0;
-	double k = -1.0;
+    double pi = 0.0;
+    double k = -1.0;
 
-	for (long i = 0; i <= N; ++i)
-	{
-		k *= -1.0;
-		pi += (4 * k) / (2 * i + 1);
-	}
+    for (long i = 0; i <= N; ++i)
+    {
+        k *= -1.0;
+        pi += (4 * k) / (2 * i + 1);
+    }
 
-	return 0;
+    return 0;
 }
